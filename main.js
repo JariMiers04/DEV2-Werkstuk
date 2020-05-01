439ce77fc5e5fdc0ab1b7992b75bdff1d038055d
Jari Miers <jarimiers@MacBook-Pro-van-Jari.local>
2020-05-01 15:38:33 +0200

    landebInladen in super Class van allData

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 // fetch inladen
 
 class allData {
-    constructor(){
-        
+    constructor() {
+
     }
     async init() {
         await this.fetch();
@@ -16,6 +16,26 @@ class allData {
     }
 }
 
+class country extends allData {
+    constructor(allData) {
+        super(allData);
+    }
+    async init() {
+        await this.fetchCountries();
+    }
+    async fetchCountries() {
+        const response = await fetch("https://api.covid19api.com/countries");
+        const json = await response.json();
+    }
+}
 
+// deel1
 const test = new allData();
+let test2 = test.init();
 console.log(test);
+console.log(test2);
+
+// landen deel2
+
+const countries = new country;
+console.log(countries);
