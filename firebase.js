42240c577f81db3be2f7f41392389446825f1433
Jari Miers <jarimiers@MacBook-Pro-van-Jari.local>
2020-05-08 21:27:29 +0200

    firebase code update

--- firebase.js
+++ firebase.js
@@ -1,14 +1,37 @@
+import main from './main.js'
+
+firebase.initializeApp({
+    apiKey: 'BwobZQQTto6nSzn9vvsp',
+    projectId: 'werkstuk-c1d86'
+});
+
 class Firebase {
-    constructor(apiKey, projectId, ) {
-        firebase.initializeApp({
-            apiKey,
-            projectId,
-        });
-        this.database = firebase.firestore();
+    constructor() {
+        this.db = firebase.firestore();
+        this.artistenCollection = this.db.collection("covid19");
     }
-    get postsCollection() {
-        return this.database.collection("country");
+    convertQuerySnapshotToRegularArray = (querySnapshot) => querySnapshot.docs.map((item) => ({
+        id: item.id,
+        ...item.data()
+    }));
+
+    async renderAll() {
+        this.globalCollection.onSnapshot((querySnapshot) => {
+            // deze callback wordt elke keer als de data veranderd terug uitgevoerd
+            const global = this.convertQuerySnapshotToRegularArray(querySnapshot);
+            console.log(global);
+        });
+    }
+
+    firebaseUpdate() {
+        this.globalCollection.add({
+            confirmed: "",
+            country: "",
+            deaths: "",
+            globalTotalCases: "test",
+            song2: "",
+            createdAt: new Date()
+        });
     }
 }
-
-export const firebasePush = new Firebase('BwobZQQTto6nSzn9vvsp', 'werkstuk-c1d86');
+export const firebasePush = new Firebase();
